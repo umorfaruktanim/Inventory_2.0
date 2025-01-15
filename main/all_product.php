5222a766677542b81f61b19a6ed885b705e8dc7a
mashiathchy <mashiath.chy18@gmail.com>
2025-01-15 22:17:27 +0600

    Mashiath

--- main/all_product.php
+++ main/all_product.php
@@ -1,15 +1,22 @@
 <?php
 include('ini/header.php');
 include('dbcon.php');
-$sql = "SELECT product.*, supplier.sup_name AS sup_name, category.cat_name FROM product 
-        LEFT JOIN supplier ON product.sup_id = supplier.id 
-        LEFT JOIN category ON product.cat_id = category.cat_id";
+
+// Updated SQL query to calculate total quantity for each code
+$sql = "SELECT p.*, 
+               s.sup_name AS sup_name, 
+               c.cat_name AS cat_name, 
+               (SELECT SUM(qty) FROM product WHERE code = p.code) AS total_qty 
+        FROM product p
+        LEFT JOIN supplier s ON p.sup_id = s.id
+        LEFT JOIN category c ON p.cat_id = c.cat_id";
 $run = mysqli_query($con, $sql);
 ?>
+
 <div class="container-fluid">
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">All Category</h6>
+            <h6 class="m-0 font-weight-bold text-primary">All Products</h6>
         </div>
         <div class="card-body">
             <div class="table-responsive">
@@ -39,24 +46,26 @@ $run = mysqli_query($con, $sql);
                         </tr>
                     </tfoot>
                     <tbody>
-                        <?php while ($data = mysqli_fetch_assoc($run)) { ?>
+                        <?php while ($data = mysqli_fetch_assoc($run)) { 
+                            $individual_qty = $data['qty'];
+
+                            $total_qty = $data['total_qty'];
+
+                            $color_class = ($total_qty < 10) ? "text-danger font-weight-bold" : "text-success font-weight-bold";
+                        ?>
                             <tr>
-                                <td><?php echo $data['name'] ?></td>
-                                <td><?php echo $data['code'] ?></td>
-                                <td><?php echo $data['cat_name'] ?></td>
-                                <td><?php echo $data['buy_price'] ?></td>
-                                <td><?php echo $data['sell_price'] ?></td>
-                                <?php if($data['qty'] < 10) { ?>
-                                	<td class="text-danger font-weight-bold"><?php echo $data['qty'] ?></td>
-                                <?php }else{ ?>
-                                <td><?php echo $data['qty'] ?></td>
-                            <?php } ?>
-                                <td><?php echo $data['sup_name'] ?></td>
+                                <td><?php echo $data['name']; ?></td>
+                                <td><?php echo $data['code']; ?></td>
+                                <td><?php echo $data['cat_name']; ?></td>
+                                <td><?php echo $data['buy_price']; ?></td>
+                                <td><?php echo $data['sell_price']; ?></td>
+                                <td class="<?php echo $color_class; ?>"><?php echo $individual_qty; ?></td>
+                                <td><?php echo $data['sup_name']; ?></td>
                                 <td>
-                                    <a href="update_stock.php?id=<?php echo $data['id'] ?>" class="btn btn-success btn-sm"><i class="fa fa-plus"></i></a>
-                                    <a href="view_product.php?id=<?php echo $data['id'] ?>" class="btn btn-info btn-sm"><i class="fa fa-eye"></i></a><br><br>
-                                    <a href="edit_product.php?id=<?php echo $data['id'] ?>" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i></a>
-                                    <a href="delete_product.php?id=<?php echo $data['id'] ?>" class="btn btn-danger btn-sm"><i class="fa fa-close"></i></a>
+                                    <a href="update_stock.php?id=<?php echo $data['id']; ?>" class="btn btn-success btn-sm"><i class="fa fa-plus"></i></a>
+                                    <a href="view_product.php?id=<?php echo $data['id']; ?>" class="btn btn-info btn-sm"><i class="fa fa-eye"></i></a><br><br>
+                                    <a href="edit_product.php?id=<?php echo $data['id']; ?>" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i></a>
+                                    <a href="delete_product.php?id=<?php echo $data['id']; ?>" class="btn btn-danger btn-sm"><i class="fa fa-close"></i></a>
                                 </td>
                             </tr>
                         <?php } ?>
